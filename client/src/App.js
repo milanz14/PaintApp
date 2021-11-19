657d4c361c585e4d3eed00b989859aecb51887d8
milanzagorac <zagoracm@gmail.com>
2021-11-19 06:06:52 -0500

    fixes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,25 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import CreatePage from './components/CreatePage';
-import Navbar from './components/Navbar';
-import Gallery from './components/Gallery';
-import Profile from './components/Profile';
-import LandingPage from './components/LandingPage';
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import CreatePage from "./components/CreatePage";
+import Navbar from "./components/Navbar";
+import Gallery from "./components/Gallery";
+import Profile from "./components/Profile";
+import LandingPage from "./components/LandingPage";
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar />
-      <div className="body-container">
-        <Routes>
-          <Route exact path="/" element={<LandingPage />} />
-          <Route exact path="/gallery" element={<Gallery />} />
-          <Route exact path="/create" element={<CreatePage />} />
-          <Route exact path="/profile" element={<Profile />} />
-        </Routes>
-      </div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Navbar />
+            <div className="body-container">
+                <Routes>
+                    <Route exact path="/" element={<LandingPage />} />
+                    <Route exact path="/gallery" element={<Gallery />} />
+                    <Route exact path="/create" element={<CreatePage />} />
+                    <Route exact path="/profile" element={<Profile />} />
+                </Routes>
+            </div>
+        </div>
+    );
 }
 
 export default App;
--- client/src/components/Gallery.js
+++ client/src/components/Gallery.js
@@ -1,33 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import '../css/Gallery.css';
-import PaintrestAPI from '../apiHandler';
-import GalleryImage from './GalleryImage';
+import React, { useState, useEffect } from "react";
+import "../css/Gallery.css";
+import PaintrestAPI from "../apiHandler";
+import GalleryImage from "./GalleryImage";
 
 const Gallery = () => {
-  const imageCount = 10;
-  const [images, setImages] = useState([]);
+    const imageCount = 10;
+    const [images, setImages] = useState([]);
 
-  useEffect(() => {
-    async function getImages() {
-      const images = await PaintrestAPI.getImages(imageCount);
-      setImages(images.posts);
-    }
-    getImages();
-  }, []);
-  console.log(images);
-  return (
-    <div>
-      <h1>Gallery of Fine Arts!</h1>
-      {images.map((image) => (
+    useEffect(() => {
+        async function getImages() {
+            const images = await PaintrestAPI.getImages(imageCount);
+            setImages(images.posts);
+        }
+        getImages();
+    }, []);
+
+    console.log(images);
+
+    return (
         <div>
-          <span key={image.id}>
-            <GalleryImage image={image.post_data} person={image.username} />
-          </span>
-          <p>Created By: {image.username}</p>
+            <h1>Gallery of Fine Arts!</h1>
+            {images.map((image) => (
+                <div>
+                    <span key={image.id}>
+                        <GalleryImage
+                            image={image.post_data}
+                            person={image.username}
+                        />
+                    </span>
+                    <p>Created By: {image.username}</p>
+                </div>
+            ))}
         </div>
-      ))}
-    </div>
-  );
+    );
 };
 
 export default Gallery;
