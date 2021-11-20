15f15074a636880df793ad402cf5038a5a7896e2
milanzagorac <zagoracm@gmail.com>
2021-11-20 18:03:50 -0500

    update API URL in handler file

--- src/apiHandler.js
+++ src/apiHandler.js
@@ -1,17 +1,17 @@
-import axios from 'axios';
-import links from './config';
+import axios from "axios";
+import links from "./config";
 
-const BASE_API_URL = process.env.REACT_APP_BASE_URL || 'http://localhost:3001';
+const BASE_API_URL = process.env.REACT_APP_BASE_URL || links.REACT_APP_BASE_URL;
 
 class PaintrestAPI {
-    static async getImages(numImages, method = 'get') {
+    static async getImages(numImages, method = "get") {
         const url = `${BASE_API_URL}/posts/count/${numImages}`;
         try {
             const images = await axios({ url, method });
             console.log(images);
             return images.data;
         } catch (err) {
-            console.error('API Error:', err.response);
+            console.error("API Error:", err.response);
             let message = err.response.data.error.message;
             throw Array.isArray(message) ? message : [message];
         }
@@ -20,9 +20,9 @@ class PaintrestAPI {
     // FIXME: need dynamic username (user must have account & be logged in)
     static async addNewImage(
         post_data,
-        username = 'user2',
-        post_name = 'testPost',
-        method = 'post'
+        username = "user2",
+        post_name = "testPost",
+        method = "post"
     ) {
         console.log(`Running Api.addNewImage`);
         const url = `${BASE_API_URL}/posts/new`;
@@ -30,29 +30,29 @@ class PaintrestAPI {
         console.log(data);
         try {
             const newPost = await axios({ url, data, method });
-            console.log('**********');
+            console.log("**********");
             console.log(newPost);
             return newPost.data;
         } catch (err) {
-            console.error('API Error:', err.response);
+            console.error("API Error:", err.response);
             let message = err.response.data.error.message;
             throw Array.isArray(message) ? message : [message];
         }
     }
 
-    static async userProfile(username, method = 'get') {
+    static async userProfile(username, method = "get") {
         const url = `${BASE_API_URL}/user/${username}`;
         try {
             const userInfo = await axios({ url, method });
             return userInfo.data;
         } catch (err) {
-            console.error('API Error:', err.response);
+            console.error("API Error:", err.response);
             let message = err.response.data.error.message;
             throw Array.isArray(message) ? message : [message];
         }
     }
 
-    static async myProfile(token, username, method = 'get') {
+    static async myProfile(token, username, method = "get") {
         const url = `${BASE_API_URL}/user/account/${username}`;
         console.log(`asdfasdf${url}afsadf`);
         try {
@@ -65,7 +65,7 @@ class PaintrestAPI {
             });
             return userInfo.data;
         } catch (err) {
-            console.error('API Error:', err.response);
+            console.error("API Error:", err.response);
             let message = err.response.data.error.message;
             throw Array.isArray(message) ? message : [message];
         }
