505da2300ce31e26b36421342c08cb6382cde9f1
milanzagorac <zagoracm@gmail.com>
2021-11-20 21:53:06 -0500

    changed data access in profile component

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,32 +1,33 @@
-import React, { useState, useEffect } from 'react';
-import PaintrestAPI from '../apiHandler';
-import '../css/Profile.css';
-import Loader from 'react-loader-spinner';
-import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
-import GalleryImage from './GalleryImage';
+import React, { useState, useEffect } from "react";
+import PaintrestAPI from "../apiHandler";
+import "../css/Profile.css";
+import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import GalleryImage from "./GalleryImage";
 
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 const Profile = () => {
     const data = useParams();
     const [images, setUser] = useState([]);
+    const [username, setUsername] = useState("");
 
     useEffect(() => {
         async function getUser() {
-            const username =
-                data.userName || sessionStorage.getItem('username');
+            const username = sessionStorage.getItem("username");
+            setUsername(username);
             const userImages = await PaintrestAPI.myProfile(username);
             setUser(userImages);
         }
         getUser();
-    }, [data.userName]);
+    }, [username]);
 
     console.log(images);
     return (
         <>
-            {data.userName ? (
+            {username ? (
                 <h3>
-                    <b>{data.userName}</b> Showroom
+                    <b>{username}'s</b> Showroom
                 </h3>
             ) : (
                 <h3>My Showroom</h3>
@@ -36,7 +37,7 @@ const Profile = () => {
                 <div className="loader">
                     <Loader
                         type="Puff"
-                        color="#00BFFF"
+                        color="#9a8c98"
                         height={120}
                         width={120}
                         timeout={3000} //3 secs
@@ -54,6 +55,9 @@ const Profile = () => {
                     ))}
                 </div>
             )}
+            <a className="btn" href="/gallery">
+                All Creations
+            </a>
         </>
     );
 };
