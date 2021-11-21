90d4bdb08d373db6406018e5959fe13ee5b7c6eb
milanzagorac <zagoracm@gmail.com>
2021-11-21 18:36:23 -0500

    fixed front end routing

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -39,17 +39,23 @@ const Navbar = () => {
             <div className="collapse navbar-collapse" id="navbarNav">
                 <ul className="navbar-nav nav-modified">
                     <li className="nav-item">
-                        <NavLink className="nav-link-modified" to="/">
+                        <NavLink className="nav-link-modified" to="/Paintrest">
                             <i className="fas fa-home"> paintrest</i>
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink className="nav-link-modified" to="/gallery">
+                        <NavLink
+                            className="nav-link-modified"
+                            to="/Paintrest/gallery"
+                        >
                             <i className="far fa-images"> Gallery</i>
                         </NavLink>
                     </li>
                     <li>
-                        <NavLink className="nav-link-modified" to="/create">
+                        <NavLink
+                            className="nav-link-modified"
+                            to="/Paintrest/create"
+                        >
                             <i className="fas fa-palette"> Create</i>
                         </NavLink>
                     </li>
@@ -58,7 +64,7 @@ const Navbar = () => {
                             <li className="nav-item">
                                 <NavLink
                                     className="nav-link-modified"
-                                    to="/profile"
+                                    to="/Paintrest/profile"
                                 >
                                     <i className="fas fa-users"> Profile</i>
                                 </NavLink>
@@ -79,7 +85,7 @@ const Navbar = () => {
                             <NavLink
                                 className="nav-link-modified"
                                 exact
-                                to="/login"
+                                to="/Paintrest/login"
                             >
                                 <i className="fas fa-sign-in-alt"> Login</i>
                             </NavLink>
