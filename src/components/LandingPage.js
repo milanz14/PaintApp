2be191bae705efaa60acd981d5222b9a2378edca
milanzagorac <zagoracm@gmail.com>
2021-11-24 14:48:39 -0500

    added information to landing page

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,26 +1,31 @@
-import React, { useContext } from 'react';
-import '../css/LandingPage.css';
-import milanImg from '../assets/milan_profile.JPG';
-import matthewImg from '../assets/matthew_profile.JPG';
-import laptopSS from '../assets/laptop.png';
-import { LoginContext } from '../helper/Context';
+import React, { useContext } from "react";
+import "../css/LandingPage.css";
+import milanImg from "../assets/milan_profile.JPG";
+import matthewImg from "../assets/matthew_profile.JPG";
+import laptopSS from "../assets/laptop.png";
+import { LoginContext } from "../helper/Context";
+import gif from "../assets/ezgif.com-gif-maker.gif";
+import nodeSS from "../assets/node.png";
+import reactSS from "../assets/React.png";
+import postgresSS from "../assets/postgres.png";
+import expressSS from "../assets/Express.png";
 
 const LandingPage = () => {
     const devInfo = [
         {
-            name: 'Milan Zagorac',
-            bio: 'Milan is a full-stack software developer based in Toronto. With a decade of experience in Sales, Business Development and a formal education in Mechanical Engineering, Milan was always fascinated with solving problems. In 2019, Milan started down a path of self-study that culminated in completing an intensive, 9-month Software Development bootcamp through Springboard in 2021. Milan is an advocate of life-long learning and is always looking forwards to keeping up with the latest in technology. Milan is proficient with JavaScript, TypeScript, React, Express, Node, Python, Flask, MongoDB and Postgres.',
-            linkedIn: 'https://www.linkedin.com/in/milanzagorac/',
-            github: 'https://www.github.com/milanz14',
-            portfolio: 'https://www.milanz.dev',
+            name: "Milan Zagorac",
+            bio: "Milan is a full-stack software developer based in Toronto. With a decade of experience in Sales, Business Development and a formal education in Mechanical Engineering, Milan was always fascinated with solving problems. In 2019, Milan started down a path of self-study that culminated in completing an intensive, 9-month Software Development bootcamp through Springboard in 2021. Milan is an advocate of life-long learning and is always looking forwards to keeping up with the latest in technology. Milan is proficient with JavaScript, TypeScript, React, Express, Node, Python, Flask, MongoDB and Postgres.",
+            linkedIn: "https://www.linkedin.com/in/milanzagorac/",
+            github: "https://www.github.com/milanz14",
+            portfolio: "https://www.milanz.dev",
             img: milanImg,
         },
         {
-            name: 'Matthew Landen',
+            name: "Matthew Landen",
             bio: "Passionate full-stack developer based in Los Angeles. Matt has nearly a decade of sales and management experience in a wide arrange of industries, including creating his own tech startup known as MechFinder between 2015-2017. Most recently he's managed $100,000+ LED lighting projects for some of the largest car dealerships in Southern California. He's spent all of his free time over the last 2+ years coding in hopes of making a long-term career transition back into the tech space. This includes completing an intensive, 9-month Software Engineering Bootcamp through Springboard in 2021!",
-            linkedIn: 'https://www.linkedin.com/in/landen1221/',
-            github: 'https://github.com/landen1221',
-            portfolio: 'https://landen1221.github.io/personal-portfolio/',
+            linkedIn: "https://www.linkedin.com/in/landen1221/",
+            github: "https://github.com/landen1221",
+            portfolio: "https://landen1221.github.io/personal-portfolio/",
             img: matthewImg,
         },
     ];
@@ -57,6 +62,59 @@ const LandingPage = () => {
                         to see.
                     </p>
                 </div>
+            </div>
+            <br />
+            <div className="container card-radius">
+                <div className="row g-1">
+                    <div className="col-lg-10 col-md-12 d-flex align-items-stretch mx-auto">
+                        <div className="card shadow p-3 mb-5 bg-body rounded card-radius">
+                            <img
+                                src={gif}
+                                className="card-img-top-card"
+                                alt="user flow"
+                            />
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    <strong>ABOUT THE APP</strong>
+                                </h5>
+                                <p className="card-text text-start">
+                                    After logging in, the user can view our
+                                    gallery of recent images or create their
+                                    own, as shown in the image above. If you
+                                    make a mistake, don't worry! You can erase
+                                    anything you don't like or completely clear
+                                    the canvas if you wish. Save your painting
+                                    to your profile when done!
+                                </p>
+                            </div>
+                            <div className="card-body">
+                                <h5 className="card-title">
+                                    <strong>TECHNOLOGIES USED</strong>
+                                </h5>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">
+                                        <p>React</p>
+                                        <img src={reactSS} />
+                                    </li>
+                                    <li className="list-group-item">
+                                        <p>Node</p>
+                                        <img src={nodeSS} />
+                                    </li>
+                                    <li className="list-group-item">
+                                        <p>Express</p>
+                                        <img src={expressSS} />
+                                    </li>
+                                    <li className="list-group-item">
+                                        <p>PostgresSQL</p>
+                                        <img src={postgresSS} />
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className="container">
                 {!loggedIn && (
                     <div className="container">
                         <br />
@@ -83,6 +141,7 @@ const LandingPage = () => {
                     </div>
                 )}
             </div>
+
             <br />
             <div className="container">
                 <h2 className="my-5">MEET THE DEVELOPERS</h2>
@@ -141,7 +200,7 @@ const LandingPage = () => {
                                 <div className="card-body">
                                     <h5 className="card-title">
                                         <strong>
-                                            {' '}
+                                            {" "}
                                             {devInfo[1].name.toUpperCase()}
                                         </strong>
                                     </h5>
